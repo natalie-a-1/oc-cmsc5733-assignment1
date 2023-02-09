94898846eade8506356a9074b739f2f851f0d064
nataliehill <natalie.a.hill-1@ou.edu>
2023-02-08 20:23:07 -0600

    created main()

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ def sum_list(numbers):
 
 def product_list(numbers):
     return numpy.product(numbers)
+
+
+def main():
+    numbers = []
+    n = int(input("Enter the list size: "))
+    for i in range(0, n):
+        numbers.append(int(input("Enter number at index " + str(i) + ": ")))
+    print("The sum is " + str(sum_list(numbers)) + ".")
+    print("The product is " + str(product_list(numbers)) + ".")
+
+
+if __name__ == '__main__':
+    main()
